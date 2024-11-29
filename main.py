a870080956bddb29d5e0df91c9efacbecedb2e90
kirey1412 <kireykayana14@gmail.com>
2024-11-29 19:30:49 +0700

    Save The Egg

--- main.py
+++ main.py
@@ -1,30 +1,40 @@
 import pgzrun, random
-
-TITLE="Save The Egg"
+TITLE="Recycle Paper Bags"
 WIDTH=800
-HEIGHT=500
+HEIGHT=600
 
 CENTER_X=WIDTH/2
 CENTER_Y=HEIGHT/2
 
 CENTER=(CENTER_X, CENTER_Y)
 gameover=False
-gamestart=True
-
-speed=10
+gamecomplete=False
 
 currentlevel=1
 finallevel=10
-items=["egg"]
+items=["whiteeggs", "yelloweggs"]
+h=[]
+
+speed=10
 
 def draw():
-    screen.blit("bground", (-200,-90))
+    screen.blit("bground", (0,0))
+    for g in h:
+        g.draw()
 
-def createitem(newitem):
-    newitem=["egg"]
-    a=Actor(str(i)+"img")
-    for i in range(currentlevel):
-        newitems.draw(newitem)
+def getitems(numberofitems):
+    newitem=["whiteeggs"]
+    for i in range(0,numberofitems):
+        randomoption=random.choice(items)
+        newitem.append(randomoption)
+    return newitem
+
+def createitems(newitem):
+    item=[]
+    for i in newitem:
+        a=Actor(str(i)+"img")
+        item.append(a)
+    return item
 
 def layout(itemstolayout):
     numberofgaps=len(itemstolayout)+1
@@ -33,30 +43,3 @@ def layout(itemstolayout):
     for index,object in enumerate(itemstolayout):
         newxposition=(index+1)*gapsize
         object.x=newxposition
-
-def makeitems(numberofitems):
-    a=getitems(numberofitems)
-    b=createitems(a)
-
-def animateitems(itemstoanimate):
-    for k in itemstoanimate:
-        duration=speed-currentlevel
-        k.anchor=("center", "bottom")
-        animation=animate(k,duration=duration,on_finished=handle_gameover,y=HEIGHT)
-        animations.append(animation)
-
-def handle_gameover():
-    global gameover
-    gameover=True
-
-if currentlevel==finallevel:
-    gamecomplete=[]
-
-
-def gamecomplete():
-    currentlevel==finallevel
-
-def update():
-    pass
-
-pgzrun.go()
